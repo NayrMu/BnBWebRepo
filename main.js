0519a6edf7da5afa68daaf66bafecbac6c52bf97
NayrMu <123273762+NayrMu@users.noreply.github.com>
2023-12-14 14:59:31 -0600

    Adds new thanksgiving images

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const dImages = ["dImages/deskBred1.jpg", "dImages/deskBred2.jpg", "dImages/deskCookies1.jpg", 
                 "dImages/deskCookies2.jpg", "dImages/deskCookies4.jpg",
-                "dImages/deskCookies5.jpg", "dImages/deskPops1.jpg"]; // Add more image URLs as needed
+                "dImages/deskCookies5.jpg", "dImages/deskPops1.jpg", "dImages/dCornCookie.jpg"]; // Add more image URLs as needed
 const pImages = ["pImages/portBred1.jpg", "pImages/portCookies1.jpg", "pImages/portCookies2.jpg", 
-                "pImages/portCookies3.jpg", "pImages/portPops1.jpg", "pImages/portPops2.jpg"]
+                "pImages/portCookies3.jpg", "pImages/portPops1.jpg", "pImages/portPops2.jpg", 
+                "pImages/pCornCookie.jpg", "pImages/pTurkeyCookie.jpg"]
 const slideshowInterval = 2500; // Change image every 3 seconds (3000 milliseconds)
 
 const displayedImage = document.getElementById('displayed-image');
